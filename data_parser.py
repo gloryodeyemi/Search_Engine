164de52410e7975a633d20f2089dd75364424a4a
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-11-30 10:33:40 -0500

    Search Engine

--- data_parser.py
+++ data_parser.py
@@ -1,9 +1,9 @@
 import os
 from whoosh.index import create_in
 from whoosh.fields import Schema, TEXT, ID, NUMERIC
-from lxml import etree  # Import the lxml library for parsing XML
+from lxml import etree
 
-# Create a schema for the search index
+# schema for the search index
 schema = Schema(
     title=TEXT(stored=True),
     authors=TEXT(stored=True),
@@ -12,14 +12,14 @@ schema = Schema(
     type=TEXT(stored=True)
 )
 
-# Create a Whoosh index
+# Whoosh index
 index_dir = "academic_papers_index"
 if not os.path.exists(index_dir):
     os.mkdir(index_dir)
 ix = create_in(index_dir, schema)
 
 
-# Function to index academic papers
+# function to index academic papers
 def index_paper(title, authors, year, url, paper_type):
     writer = ix.writer()
     writer.add_document(
@@ -32,19 +32,19 @@ def index_paper(title, authors, year, url, paper_type):
     writer.commit()
 
 
-# Function to parse and index an entry
+# function to parse and index an entry
 def parse_and_index_entry(entry):
     title = entry.findtext('title')
     authors = [author.text for author in entry.xpath('author')]
     year = int(entry.findtext('year'))
 
-    # Handle both types of URL elements
+    # handle both types of URL elements
     url = entry.findtext('ee[@type="oa"]')
 
     if url is None:
         url = entry.findtext('url')
 
-    # Handle articles without a URL
+    # handle articles without a URL
     if url is None:
         url = "URL not available"
 
@@ -53,25 +53,25 @@ def parse_and_index_entry(entry):
     index_paper(title, ", ".join(authors), year, url, paper_type)
 
 
-# Function to fetch and index academic papers from a source
+# function to fetch and index academic papers from a source
 def fetch_and_index_papers(xml_file):
-    # Specify the DTD file to be used during parsing
+    # specify the DTD file to be used during parsing
     dtd_file = 'dblp.dtd'
 
-    # Create a custom parser with DTD validation
+    # create a custom parser with DTD validation
     parser = etree.XMLParser(dtd_validation=True, no_network=False)
 
-    # Parse the XML document and specify the DTD file
+    # parse the XML document and specify the DTD file
     tree = etree.parse(xml_file, parser=parser)
 
-    # Get all entries from the XML (articles, books, inproceedings, etc.)
+    # get all entries from the XML (articles, books, inproceedings, etc.)
     entries = tree.xpath('//article | //book | //inproceedings | //www | //phdthesis | //proceedings | //incollection |'
                          ' //mastersthesis')
 
-    # Iterate through articles within the specified range
+    # iterate through articles within the specified range
     for entry in entries:
         parse_and_index_entry(entry)
 
 
 if __name__ == '__main__':
-    fetch_and_index_papers('dblp.xml')  # Fetch and index papers on startup
+    fetch_and_index_papers('dblp.xml')  # fetch and index papers on startup
